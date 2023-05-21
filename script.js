10e61e1c2dbd4ec22d980c9cee209209c74f2a2a
adrian-guereque <124836243+adrianguereque@users.noreply.github.com>
2023-05-21 17:05:24 -0600

    Calculate box width and height, made functions

---
+++ script.js
@@ -0,0 +1,24 @@
+const grid = document.querySelector("#grid");
+
+function createBox(){
+    const box = document.createElement("div");
+    box.classList.add('box');
+    box.addEventListener("mouseenter",() =>{
+        box.classList.add('boxHover');
+    })
+    grid.appendChild(box);
+}
+function createGrid(){
+    for(let i=0;i<20;i++){
+        for(let j=0;j<20;j++){
+            createBox();
+        }
+    }
+}
+function boxParamaters(num){
+    const boxClass = document.getElementsByClassName("box");
+    let side = Math.sqrt((360000/(num*num))) - 4;
+    boxClass.style.width = side;
+    boxClass.style.height = side;
+}
+createGrid();
